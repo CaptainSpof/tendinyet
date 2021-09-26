1dd21489fe04ec8a0d1d8415092efd4c0571962d
CaptainSpof <captain.spof@gmail.com>
2021-09-26 22:25:03 +0200

    ✨ feat(tendinyet): add first draft

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use random_ramble::refactor::RandomRamble;
+
+use notify_rust::Notification;
+
+static SOUND: &str = "message-new-instant";
+
+fn main() {
+    let timeout = 20;
+
+    let summary = RandomRamble::new()
+        .with_template("{{ intro | rr }}\n{{ desc | rr }}")
+        .with_others(
+            "intro",
+            vec!["Stop right there, criminal scum!", "<snarky remark here>"],
+        )
+        .with_others(
+            "desc",
+            vec![
+                "Get up! Lazy <noun>",
+                "You'll die if you don't move!",
+                "You've been pretending to work for like 20 minutes! Take it easy.",
+                "Hands off the keyboard.",
+                "Hands up!! Now, wave them around, like you just don't care",
+                "Something method Pomodoro, something…",
+                "You remind me of the humans in Wall-E… GET UP!",
+            ],
+        )
+        .build()
+        .unwrap()
+        .to_string();
+
+    let btn_txt = RandomRamble::new()
+        .with_template("{{ btn | rr }}")
+        .with_others(
+            "btn",
+            vec![
+                "Feck off",
+                "Leave me alone!",
+                "Honey, I'm not in the mood.",
+                "SNOOZE!!!!",
+                "OMG, I don't care",
+                "Shh, only dreams now…",
+            ],
+        )
+        .build()
+        .unwrap()
+        .to_string();
+
+    #[cfg(all(unix, not(target_os = "macos")))]
+    Notification::new()
+        .icon("chronometer")
+        .sound_name(SOUND)
+        .summary(&summary)
+        .appname("Get up, NERD!")
+        .action("snooze", &btn_txt)
+        .timeout(timeout * 1000)
+        .show()
+        .unwrap()
+        .wait_for_action(|action| match action {
+            "default" => println!("default"),
+            "snooze" => println!("<put code here>"),
+            // FIXME: here "__closed" is a hardcoded keyword, it will be deprecated!!
+            "__closed" => println!("the notification was closed"),
+            _ => (),
+        });
+
+    Notification::new()
+        .appname("Back to work, NERD!")
+        .icon("display-symbolic")
+        .summary("Alright, back to work.")
+        .timeout(3 * 1000)
+        .show()
+        .unwrap();
+}
